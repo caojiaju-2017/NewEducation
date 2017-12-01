6e09cfe666d8589427a0a9c6d7f817e2ab4cd0af
caojiaju-2017 <jiaju_cao@hotmail.com>
2017-12-02 05:56:22 +0800

    1
    1

---
+++ 4.Code/Education/include.py
@@ -0,0 +1,35 @@
+#!/usr/local/bin/python
+#-*- coding:utf-8 -*-
+
+import httplib
+import urllib
+import datetime
+import threading
+import md5
+import random
+import os
+import inspect
+import hashlib
+import base64
+import pickle
+import socket
+import sys
+import platform
+import time
+import json
+import uuid
+import re
+import qrcode
+import urllib2
+
+
+from HsPlatform.Sms.HsSmsHelp import *
+from HsPlatform.Sms.MobileRecord import *
+
+# DJANGO 引用
+from django.shortcuts import render,render_to_response
+from django.http import  HttpResponse
+from django.db import transaction
+from django.views.decorators.csrf import csrf_exempt
+from django.db.models import F
+from django.template import Template, Context
---
+++ 4.Code/EducationOld/HsEdu/static/js/nav4.js
@@ -0,0 +1,13 @@
+// parent.document.all("frame_main").style.height=document.body.scrollHeight;
+// window.onload=function()
+// {
+//
+// };
+//
+// $(document).ready(function()
+// {
+//
+// });
+
+
+
